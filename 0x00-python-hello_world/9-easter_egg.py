d34eb14b601d366d044c6cc9bcebd73c729f09ba
Tobey14 <sureboytobi@gmail.com>
2022-11-29 12:16:31 +0100

    New additions

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -9,16 +9,6 @@ str = (
     f'Sparse is better than dense.\n'
     f'Readability counts.\n'
     f'Special cases aren\'t special enough to break the rules.\n'
-    f'Although practicality beats purity.\n'
-    f'Errors should never pass silently.\n'
-    f'Unless explicitly silenced.\n'
-    f'In the face of ambiguity, refuse the temptation to guess.\n'
-    f'There should be one-- and preferably only one --obvious way to do it.\n'
-    f'Although that way may not be obvious at first unless you\'re Dutch.\n'
-    f'Now is better than never.\n'
-    f'Although never is often better than *right* now.\n'
-    f'If the implementation is hard to explain, it\'s a bad idea.\n'
-    f'If the implementation is easy to explain, it may be a good idea.\n'
-    f'Namespaces are one honking great idea -- let\'s do more of those!'
+    
 )
 print(str)
