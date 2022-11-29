0a0f286d0b6806628faf10523dedf0e178f2577f
Tobey14 <sureboytobi@gmail.com>
2022-11-29 12:18:40 +0100

    changes

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 str = (
-    f'The Zen of Python, by Tim Peters \n\n'
-    f'Beautiful is better than ugly.\n'
-    f'Explicit is better than implicit.\n'    
+    f'The Zen of Python, by Tim Peters \n\n'   
 )
 print(str)
