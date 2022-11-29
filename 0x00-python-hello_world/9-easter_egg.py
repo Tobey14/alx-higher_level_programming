85cf482104e1855da47aeaba37cc205a19da00a6
Tobey14 <sureboytobi@gmail.com>
2022-11-29 12:17:36 +0100

    changes

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -2,13 +2,6 @@
 str = (
     f'The Zen of Python, by Tim Peters \n\n'
     f'Beautiful is better than ugly.\n'
-    f'Explicit is better than implicit.\n'
-    f'Simple is better than complex.\n'
-    f'Complex is better than complicated.\n'
-    f'Flat is better than nested.\n'
-    f'Sparse is better than dense.\n'
-    f'Readability counts.\n'
-    f'Special cases aren\'t special enough to break the rules.\n'
-    
+    f'Explicit is better than implicit.\n'    
 )
 print(str)
