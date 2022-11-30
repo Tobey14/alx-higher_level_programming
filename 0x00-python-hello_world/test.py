75874fb99fd8bdca28157d7a30710ce809b4757f
Tobey14 <sureboytobi@gmail.com>
2022-11-30 01:09:19 +0100

    Combo

--- 0x00-python-hello_world/test.py
+++ 0x00-python-hello_world/test.py
@@ -2,3 +2,13 @@
 for i in range(2, 10, 8):
     print(i, end=" ")
 
+#!/usr/bin/python3
+not_print = (10, 20, 30, 40, 50, 60, 70, 80)
+for num in range(0, 90):
+    if num in not_print:
+        continue
+    else:
+        if num != 89:
+            print("{:02d}, ".format(num), end='')
+        else:
+            print("{:02d}\n".format(num))
