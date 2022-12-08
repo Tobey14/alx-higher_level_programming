db01e44523a0e3857091aedf6daccf0969a5eaf7
Tobey14 <sureboytobi@gmail.com>
2022-12-08 14:53:48 +0100

    Complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for index in list_keys:
+        if value == a_dictionary[index]:
+            del a_dictionary[index]
+
+    return (a_dictionary)
