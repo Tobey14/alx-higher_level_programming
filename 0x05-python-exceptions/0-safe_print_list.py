a625d71154bc024fcfab18efa69d19073c0d7519
Tobey14 <sureboytobi@gmail.com>
2022-12-19 23:07:07 +0100

    Fixes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        count = 0
-        for i in range(x):
-            print(my_list[i], end='')
-            count += 1
-        print(' ')
-        return count
-    except Exception:
-        return
+        cont = 0
+        for index in range(0, x):
+            cont = cont + 1
+            print('{}'.format(my_list[index]), end='')
+        print()
+        return x
+    except IndexError:
+        print()
+        return index
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
-            if(type(my_list[i]) is int):
-                print(my_list[i], end='')
-                count += 1
-        print(' ')
-        return count
-    except IndexError:
-        print('IndexError: list index out of range')
+    cont = 0
+    for index in range(0, x):
+        try:
+            print('{:d}'.format(my_list[index]), end='')
+            cont = cont + 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return cont
