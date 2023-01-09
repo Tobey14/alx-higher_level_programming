6dffd082540163894b0a9a0d2437b6a4a7a30941
Tobey14 <sureboytobi@gmail.com>
2023-01-09 20:01:37 +0100

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining a function lookup"""
+
+
+def lookup(obj):
+    """returns list of objects in obj"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+# alx-higher_level_programming
+Repository For ALX Higher level programming python inheritance tasks
