9b4e2241b437110ccdc822b5404a3ee9aab03d45
Onuigbo Tobechukwu Collins <sureboytobi@gmail.com>
2023-03-19 21:08:47 +0100

    Fixes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """
-Safe from MySQL injections
-Takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,7 +11,13 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True,
-                unique=True, nullable=False, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
